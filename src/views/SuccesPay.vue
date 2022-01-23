2036dee360335d463cfb576afd35837c66bd0cc4
Lapxi010 <denisova.www2020@gmail.com>
2022-01-23 20:06:13 +0300

    I'm change color theme

--- src/views/SuccesPay.vue
+++ src/views/SuccesPay.vue
@@ -1,10 +1,31 @@
 <template>
-  <h1>Успешно проведена оплата</h1>
+  <TheBreadcrumbs :breads="breads"></TheBreadcrumbs>
+  <div class="catalog">
+    <div class="limit">
+      <div class="payment">
+        <div class="payment__block">
+          <h1>Успешно проведена оплата</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TheBreadcrumbs from "../components/TheBreadcrumbs"
+
 export default {
-  name: "SuccesPay"
+  name: "SuccesPay",
+  components:{TheBreadcrumbs},
+  setup(){
+    const breads = [
+      {name:'Главная',path:'/'},
+      {name:'Проведения оплаты',path:'#'}
+    ]
+    return {
+      breads
+    }
+  }
 }
 </script>
 
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,31 @@
 <template>
-
+  <TheBreadcrumbs :breads="breads"></TheBreadcrumbs>
+  <div class="catalog">
+    <div class="limit">
+      <div class="payment">
+        <div class="payment__block">
+          <h1>Успешно проведена оплата</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import TheBreadcrumbs from "../components/TheBreadcrumbs";
+
 export default {
-  name: "User"
+  name: "User",
+  components:{TheBreadcrumbs},
+  setup(){
+    const breads = [
+      {name:'Главная',path:'/'},
+      {name:'Личный кабинет',path:'#'}
+    ]
+    return {
+      breads
+    }
+  }
 }
 </script>
 
